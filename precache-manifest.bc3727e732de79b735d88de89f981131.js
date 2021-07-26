466a63df3f37bd2733bbe8f5bfb43a1a35d6b491
paul-nguyen96 <paul121596@icloud.com>
2021-07-25 20:05:42 -0500

    Updates

---
+++ precache-manifest.bc3727e732de79b735d88de89f981131.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a07b79dbdc28003b8a72ff8e00fa7e2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2e6ce55e9138d4dc856f",
+    "url": "/portfolio/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "ab18e2081725bf99f393",
+    "url": "/portfolio/static/js/2.4b152933.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portfolio/static/js/2.4b152933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e6ce55e9138d4dc856f",
+    "url": "/portfolio/static/js/main.c138112c.chunk.js"
+  },
+  {
+    "revision": "7586950e9bfddf720b57",
+    "url": "/portfolio/static/js/runtime-main.1641258c.js"
+  },
+  {
+    "revision": "9d113e7a9832404d82230e4f3b53fef7",
+    "url": "/portfolio/static/media/paul.9d113e7a.jpg"
+  },
+  {
+    "revision": "b57f37031feedf48249fd5cdbb5f688a",
+    "url": "/portfolio/static/media/resume.b57f3703.pdf"
+  }
+]);
